84ad23508c9127c497a429fe68697ac348589e3e
smandler <sv.mandler@gmail.com>
2016-08-03 12:44:34 +1000

    start

---
+++ Assets/Scripts/PictureLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PictureLoad : MonoBehaviour {
+    public string url = Application.dataPath + "/snake.jpg"; // "C:/usc/home/smandler/Desktop/GitHub/usc_cave/snake.jpg";
+    private WWW ww;
+
+    IEnumerator Start()
+    {
+        ww = new WWW(url);
+        yield return ww;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.mainTexture = ww.texture;
+    }
+
+}
